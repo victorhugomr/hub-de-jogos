7012dc1c0bc9aacedb265e70279494b8dafccc9f
victorhugomr <victorhugo_mr@live.com>
2023-01-20 17:09:55 -0300

    Add Knight piece

--- Models/Chess/Pieces/Knight.cs
+++ Models/Chess/Pieces/Knight.cs
@@ -6,13 +6,35 @@ namespace hubdejogos.Models.Chess.Pieces{
         }
 
         public override bool moveValidate(Board board, int destinyLine, int destinyColumn){
-           /*  if(){
-                
+            if(board.SelectedPiece != null){
+                if(Math.Abs(board.SelectedPiece.Line - destinyLine) == 1){
+                    if(Math.Abs(board.SelectedPiece.Column - destinyColumn) != 2){
+                        return false;
+                    }
+                    if(board.Position[destinyLine,destinyColumn] != null){
+                        if(board.Position[destinyLine,destinyColumn].Color == board.SelectedPiece.Color){
+                            return false;
+                        }
+                    }
+                }
+                else if(Math.Abs(board.SelectedPiece.Line - destinyLine) == 2){
+                    if(Math.Abs(board.SelectedPiece.Column - destinyColumn) != 1){
+                        return false;
+                    }
+                    if(board.Position[destinyLine,destinyColumn] != null){
+                        if(board.Position[destinyLine,destinyColumn].Color == board.SelectedPiece.Color){
+                            return false;
+                        }
+                    }
+                }
+                else{
+                    return false;
+                }
             }
             //Verifica se o destino é igual a origem
-            if(board.SelectedPiece.Line == destinyLine && board.SelectedPiece.Column == destinyColumn){
+            if(board.SelectedPiece?.Line == destinyLine && board.SelectedPiece.Column == destinyColumn){
                 return false;
-            } */
+            }
 
             return true;
         }
